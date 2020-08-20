e509c8820520e85a7662a8381594626a8cb922b0
Federico Baron <federicoagustinbaron@gmail.com>
2020-08-20 17:20:51 -0400

    init project

---
+++ PlayListy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PlayListy
+//
+//  Created by Federico Baron on 8/20/20.
+//  Copyright © 2020 Federico Baron. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
